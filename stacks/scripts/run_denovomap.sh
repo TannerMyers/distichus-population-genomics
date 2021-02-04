e9243e1947c3eaef78dd94143f5e929874b7de5e
Tanner C. Myers <tannercmyers@gmail.com>
2021-02-04 08:10:11 -0600

    iDownlokbkbkbkbkbkbAdding up-to-dkbkbkb-kbkbkb to date and working version of shell script to submit the Stacks wrapper `denovo_map.pl'.The shekbkbkbkbkbis shell script is meant to compare stakbkbkbdenovo_map outputs depending on Stakbkbkbthe Stacks parameters NkbM and n.You run it by submitting as a script on the command line kb, kbkb ikb(e.g.kbkbkbkbi.e.gkb, ./run_denovomap.sh 1) with the valuefor M and n as a command line variable. A her dkbkbe document in th kbe script does the work of submitting thedenovo_map.pl command as a job to Auburn's kbkbkbkbkbkbkbkbkba PBS job submission queue kb.:wq:w cokbkbAdding run_denovomap.sh:21:1kb21kbkbwq

--- stacks/scripts/run_denovomap.sh
+++ stacks/scripts/run_denovomap.sh
@@ -1,45 +1,44 @@
-#! /bin/bash
+#!/usr/bin/env bash 
 
-# Update once version that works is ID'd
-module load stacks 
+M=$1
 
 # Name --popmap variable
-popmap=/scratch/phyletica/distichus/info/popmap.test_samples.tsv
+POP_MAP=/scratch/phyletica/distichus/info/popmap.test_samples.tsv
 
 # Name --samples variable
-samples=/scratch/phyletica/distichus/samples 
+SAMPLES=/scratch/phyletica/distichus/samples 
+
+# Provide job submission name
+JOB_NAME=denovomaptest
 
 # Make an array for the values of M to test
-M=( 1 2 3 4 5 6 7 8 9 )
-
-# Name job to be submitted with qsub
-jobname=denovomaptest
+#M=( 1 2 3 4 5 6 7 8 9 )
 
 	# iterate over the array, M
-	for M in ${M[@]}; 
-			do
+#	for NUMBER in ${M[@]}; 
+#			do
+				# Name variable for output files 
+		       		OUT_DIR=/scratch/phyletica/distichus/tests.denovo/stacks.M$M 
+				# Move into directory for output files
+				cd $OUT_DIR
 
-		# Name variable for output files 
-        out_dir=/scratch/phyletica/distichus/tests.denovo/stacks.M$M 
-		# Move into directory for output files
-		cd $out_dir
-
-		qsub -N $jobname$M \
-                -q general \
-                -W group_list=jro0014_lab \
-                -W x=FLAGS:ADVRES:jro0014_lab \
-                -d $out_dir \
-                 -l walltime=48:00:00 << HereDoc
+			qsub -N $JOB_NAME$M \
+                         -q general \
+                         -W group_list=jro0014_lab \
+                         -W x=FLAGS:ADVRES:jro0014_lab \
+                         -d /scratch/phyletica/distichus/samples/ \
+                         -l walltime=48:00:00 << HereDoc
+				
+				module load stacks
 
 				# run denovo_map for those samples for the value of M being tested
-					denovo_map.pl \
+						denovo_map.pl \
 							-M $M \
 							-n $M \
-							-m 3 \
-							-o $out_dir \
-							--popmap $popmap \
-							--samples $samples \
+							--out-path $OUT_DIR \
+							--popmap $POP_MAP \
+							--samples $SAMPLES \
 							--paired &> denovomap.oe 	
-		
-HereDoc			
-			done
+
+HereDoc				
+
