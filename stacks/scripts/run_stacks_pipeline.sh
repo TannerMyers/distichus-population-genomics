f825dbcef1cf09e1fa7b3799fef093ea526abae0
Tanner C. Myers <tannercmyers@gmail.com>
2021-03-25 14:11:28 -0500

    Adding current version of script; commented out code to focus on running populations

--- stacks/scripts/run_stacks_pipeline.sh
+++ stacks/scripts/run_stacks_pipeline.sh
@@ -7,55 +7,52 @@
 ## Set command line arguments for M and n -- the number of mismatches allowed 
 ## between stacks within individuals and mismatches allowed between stacks
 ## between individuals
-M=$1
-n=$2 
-POP_MAP=$3
-OUT_DIR=$4
+#M=$1
+#n=$2 
+POP_MAP=$1
+OUT_DIR=$2
 
-SAMPLE_DIR=/scratch/phyletica/distichus/samples
+#SAMPLE_DIR=/scratch/phyletica/distichus/samples
 
-SAMPLES=`awk 'BEGIN {OFS = FS} {print $1}' $POP_MAP` 
+#SAMPLES=`awk 'BEGIN {OFS = FS} {print $1}' $POP_MAP` 
 
 # Load 
 module load stacks
 
-id=1
-for sample in $SAMPLES 
-do
-	ustacks -f $SAMPLE_DIR/${sample}.1.fq.gz -i $id --name $sample -o $OUT_DIR -M $M -m $n -N $n --disable-gapped -p 8
-	let "id+=1"
-done
+#id=1
+#for sample in $SAMPLES 
+#do
+#	ustacks -f $SAMPLE_DIR/${sample}.1.fq.gz -i $id --name $sample -o $OUT_DIR -M $M -m $n -N $n --disable-gapped -p 8
+#	let "id+=1"
+#done
 
-cstacks -P $OUT_DIR \
-	-M $POP_MAP \
-	-n $n \
-	-p 8	
+#cstacks -P $OUT_DIR \
+#	-M $POP_MAP \
+#	-n $n \
+#	-p 8	
 
-sstacks -P $OUT_DIR \
-	-M $POP_MAP \
-	-p 8 
+#sstacks -P $OUT_DIR \
+#	-M $POP_MAP \
+#	-p 8 
 
-tsv2bam -P $OUT_DIR \
-        -M $POP_MAP \
-	--pe-reads-dir $SAMPLE_DIR \
-        -t 8 
+#tsv2bam -P $OUT_DIR \
+#        -M $POP_MAP \
+#	--pe-reads-dir $SAMPLE_DIR \
+#        -t 8 
 
-gstacks -P $OUT_DIR \
-	-M $POP_MAP \
-        -t 8 
+#gstacks -P $OUT_DIR \
+#	-M $POP_MAP \
+#        -t 8 
 
-#populations -P /scratch/phyletica/distichus/stacks.denovo/uncleaned \
-#	-M /scratch/phyletica/distichus/info/popmap.tsv \
-#	-t 8 \
-#	-R 0.65 \
-#	--min-mac 2 \
-#	--write-single-snp \
-#	--fasta-samples \
-#	--fasta-loci \
-#	--vcf \
+populations -P $OUT_DIR -M $POP_MAP -t 8 --vcf 
+
+#       -R 0.65 \
+#       --min-mac 2 \
+#       --write-single-snp \
+#       --fasta-samples \
+#       --fasta-loci \
 #	--structure \
 #	--hwe \
 #	--fstats \
 #	--phylip-var \
-#	--plink
-	 
+#	--plink	 
