296dad4d2b6289a6bd00ea555af3e835fd40b13a
Tanner C. Myers <tannercmyers@gmail.com>
2021-03-28 20:46:39 -0500

    Adding reference genome alignment and integration parts of pipeline

--- stacks/scripts/run_stacks_pipeline.sh
+++ stacks/scripts/run_stacks_pipeline.sh
@@ -7,45 +7,58 @@
 ## Set command line arguments for M and n -- the number of mismatches allowed 
 ## between stacks within individuals and mismatches allowed between stacks
 ## between individuals
-#M=$1
-#n=$2 
-POP_MAP=$1
-OUT_DIR=$2
+M=$1
+n=$2 
+POP_MAP=$3
+OUT_DIR=$4
 
-#SAMPLE_DIR=/scratch/phyletica/distichus/samples
+SAMPLE_DIR=/scratch/phyletica/distichus/samples
 
-#SAMPLES=`awk 'BEGIN {OFS = FS} {print $1}' $POP_MAP` 
+SAMPLES=`awk 'BEGIN {OFS = FS} {print $1}' $POP_MAP`
 
-# Load 
+BWA_DB=/scratch/phyletica/distichus/genome/bwa/anocar
+
+# Load necessary modules 
 module load stacks
+module load python
+module load samtools
+module load bwa
 
-#id=1
-#for sample in $SAMPLES 
-#do
-#	ustacks -f $SAMPLE_DIR/${sample}.1.fq.gz -i $id --name $sample -o $OUT_DIR -M $M -m $n -N $n --disable-gapped -p 8
-#	let "id+=1"
-#done
+id=1
+for sample in $SAMPLES 
+do
+	ustacks -f $SAMPLE_DIR/${sample}.1.fq.gz -i $id --name $sample -o $OUT_DIR -M $M -m $n -N $n --disable-gapped -p 8
+	let "id+=1"
+done
 
-#cstacks -P $OUT_DIR \
-#	-M $POP_MAP \
-#	-n $n \
-#	-p 8	
+cstacks -P $OUT_DIR \
+	-M $POP_MAP \
+	-n $n \
+	-p 8	
 
-#sstacks -P $OUT_DIR \
-#	-M $POP_MAP \
-#	-p 8 
+sstacks -P $OUT_DIR \
+	-M $POP_MAP \
+	-p 8 
 
-#tsv2bam -P $OUT_DIR \
-#        -M $POP_MAP \
-#	--pe-reads-dir $SAMPLE_DIR \
-#        -t 8 
+tsv2bam -P $OUT_DIR \
+        -M $POP_MAP \
+	--pe-reads-dir $SAMPLE_DIR \
+        -t 8 
 
-#gstacks -P $OUT_DIR \
-#	-M $POP_MAP \
-#        -t 8 
+gstacks -P $OUT_DIR \
+	-M $POP_MAP \
+        -t 8 
+
+bwa mem -t 8 $BWA_DB $OUT_DIR/catalog.fa.gz |
+    samtools view -b |
+    samtools sort --threads 4 > $OUT_DIR/aligned_catalog.bam
+
+# Confused about what "stacks_dir" refers to with the "-P" flag
+stacks-integrate-alignments -P $OUT_DIR \
+    -B $OUT_DIR/aligned_catalog.bam \
+    -O $OUT_DIR/integrated-alignment
 
 populations -P $OUT_DIR -M $POP_MAP -t 8 --vcf 
-
 #       -R 0.65 \
 #       --min-mac 2 \
 #       --write-single-snp \
