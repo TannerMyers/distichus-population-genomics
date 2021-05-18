140bed8d856c5c1b7dc4f72e63fcd4173d6c5263
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-18 15:30:07 -0500

    Adding script to plot PCA results from plink with R

---
+++ population-structure/scripts/plink-PCA-results.R
@@ -0,0 +1,10 @@
+#####################################################################
+#   This script plots the results of Principal Component Analysis   #
+#   performed by plink using the flag `--pca`                       #
+#####################################################################
+
+setwd() # change to your working directory
+
+################# Load libraries  #################
+library(tidyverse)
+library(ggplot2)
