09ab11b3458e189fd733dea29c6f5eb320c0f3b9
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-21 11:44:11 -0500

    Redirect cross-validation results to a file

--- population-structure/scripts/run_admixture.sh
+++ population-structure/scripts/run_admixture.sh
@@ -7,10 +7,12 @@ conda activate population-genomics
 # Name variables
 INPUT_FILE=$1
 
+CV_LOG=$2
+
 # Run Admixture on a number of "K" value.
 for K in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; 
 	do
 	    # Run admixture with cross validation
-	    admixture --cv $INPUT_FILE $K | tee log${K}.out
+	    admixture --cv $INPUT_FILE $K | tee log${K}.out >> $CV_LOG
 
 	done
