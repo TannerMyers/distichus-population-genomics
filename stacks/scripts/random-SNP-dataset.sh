9658505f28542a5578ab521a8b91f895b613b9ad
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-21 12:15:39 -0500

    Updating random SNP dataset generating script with new pseudocode

--- stacks/scripts/random-SNP-dataset.sh
+++ stacks/scripts/random-SNP-dataset.sh
@@ -1,9 +1,33 @@
-[[ -d $out_folder/run_%j ]] || mkdir -p $out_folder/run_%j # the %j is slurm for the job number -- figure out how to do this with PBS
+#!/usr/bin/env bash
 
-populations \
-	--write-random-snp \
-	--min-mac 2
+# Load modules
+module load stacks
 
-cp $out_folder/populations.structure $structure_folder/populations.structure.%j
+# Name variables
+OUT_DIR=$1
+POPULATIONS_DIR=$2
+POP_MAP=$3
 
 
+
+[[ -d $POPULATIONS_DIR/run_%j ]] || mkdir -p $POPULATIONS_DIR/run_%j 
+# the %j is slurm for the job number -- figure out how to do this with PBS
+# I'm just going to use 1-100 and use that for iterating my for loop and naming temporary directories to contain outputs
+
+
+for ; do
+
+qsub
+
+	populations --in-path $OUT_DIR \
+		--out-path $POPULATIONS_DIR \
+		--popmap $POP_MAP \
+		--threads \
+		--ordered-export \
+		--write-random-snp \
+		--min-mac 2 \
+		--vcf --plink 
+
+	cp $POPULATIONS_DIR/populations.structure $structure_folder/populations.structure.%j
+
+done
