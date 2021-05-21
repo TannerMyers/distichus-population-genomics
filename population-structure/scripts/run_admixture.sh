0fd7ccacd59ad40c5049e1e564ae1cb67aa17019
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-21 11:16:13 -0500

    Adding line of code to activate conda environment with Admixture v.1.3.0

--- population-structure/scripts/run_admixture.sh
+++ population-structure/scripts/run_admixture.sh
@@ -1,17 +1,16 @@
 #!/usr/bin/env bash
 
-module load R/3.6.3
+# Load conda environment containing Admixture v. 1.3.0
+source ~/miniconda3/etc/profile.d/conda.sh # Or path to where your conda is
+conda activate population-genomics
 
 # Name variables
 INPUT_FILE=$1
 
 # Run Admixture on a number of "K" value.
-for K in 1 2 3 4 5 6 7 8 9 10; 
+for K in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; 
 	do
 	    # Run admixture with cross validation
 	    admixture --cv $INPUT_FILE $K | tee log${K}.out
 
 	done
-
-# Run R plotting script to visualize Q estimates.
-Rscript plot_Admixture.r
