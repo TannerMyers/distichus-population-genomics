9484be0873d9c6b5a9de7de385dac52509c67d4a
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-21 16:44:14 -0500

    Updating scripts

--- stacks/scripts/random-SNP-dataset.sh
+++ stacks/scripts/random-SNP-dataset.sh
@@ -1,33 +1,42 @@
 #!/usr/bin/env bash
 
-# Load modules
-module load stacks
-
 # Name variables
-OUT_DIR=$1
-POPULATIONS_DIR=$2
-POP_MAP=$3
+INPUT_DIR=/scratch/phyletica/distichus/stacks.denovo/uncleaned.M4m3N3/
+POP_MAP=/scratch/phyletica/distichus/info/popmap_cleaned_MacGuigan_no-brev_inds-as-pops.tsv
+POPULATIONS_DIR=/scratch/phyletica/distichus/population-structure-datasets
 
+for ((i=1; i<=100;i++)); 
+	do
 
+		qsub -N admixture-dataset$i \
+			-n -d /scratch/phyletica/distichus/scripts \
+			-q gen28 \
+			-W group_list=jro0014_lab \
+			-W x=FLAGS:ADVRES:jro0014_s28 \
+			-l nodes=1:ppn=20,mem=100gb,walltime=24:00:00 <<<"
 
-[[ -d $POPULATIONS_DIR/run_%j ]] || mkdir -p $POPULATIONS_DIR/run_%j 
-# the %j is slurm for the job number -- figure out how to do this with PBS
-# I'm just going to use 1-100 and use that for iterating my for loop and naming temporary directories to contain outputs
+				# Load modules
+				module load stacks
 
+				# Make a directory for this populations run's number 
+				[[ -d $POPULATIONS_DIR/Run$i ]] || mkdir -p $POPULATIONS_DIR/Run$i 
 
-for ; do
+				# Run populations to get plink (.ped) and .vcf files
+				populations --in-path $INPUT_DIR \
+					--out-path $POPULATIONS_DIR/Run$i \
+					--popmap $POP_MAP \
+					--threads \
+					-R 0.7 \
+					--ordered-export \
+					--write-random-snp \
+					--min-mac 2 \
+					--vcf --plink --structure
 
-qsub
-
-	populations --in-path $OUT_DIR \
-		--out-path $POPULATIONS_DIR \
-		--popmap $POP_MAP \
-		--threads \
-		--ordered-export \
-		--write-random-snp \
-		--min-mac 2 \
-		--vcf --plink 
-
-	cp $POPULATIONS_DIR/populations.structure $structure_folder/populations.structure.%j
-
-done
+				# Copy populations output files to directory that will contain the `populations`
+				# output files for all datasets
+				cp $Run$i/populations.snps.vcf $POPULATIONS_DIR/Run$i.populations.snps.vcf
+				cp $Run$i/populations.plink.ped $POPULATIONS_DIR/Run$i.populations.plink.ped
+				cp $Run$i/populations.plink.map $POPULATIONS_DIR/Run$i.populations.plink.map
+				cp $Run$i/populations.structure $POPULATIONS_DIR/Run$i.populations.structure
+				"
+	done
