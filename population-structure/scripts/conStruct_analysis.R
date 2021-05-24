7e7fe16995d325ee96a16de58028fe0305c0c396
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-24 17:18:49 -0500

    Adding script to perform spatial population structure analysis with conStruct

---
+++ population-structure/scripts/conStruct_analysis.R
@@ -0,0 +1,4 @@
+myconStructdata <- structure2conStruct(infile = "populations.distichusonly.R0.7.str", 
+                                       onerowperind = FALSE, start.loci = 3, 
+                                       start.samples = 2, missing.datum = -9, 
+                                       outfile = "myconStructdata")
