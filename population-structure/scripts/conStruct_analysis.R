fe92a66e097e44d33a862cc0e5fecbdac990848a
Tanner C. Myers <tannercmyers@gmail.com>
2021-05-28 17:35:36 -0500

    Adding R and bash scripts to submit conStruct analyses on Hopper

--- population-structure/scripts/conStruct_analysis.R
+++ population-structure/scripts/conStruct_analysis.R
@@ -12,35 +12,44 @@
 
 ###############
 
-#setwd("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/population-structure/conStruct")
+# Change to your working directory
+setwd("/gpfs01/home/tcm0036/distichus/population-structure/conStruct")
 
-# Load packages
-# Include the "lib.loc" argument is necessary to tell R on the cluster where to look for the installed package
-library(optparse, lib.loc="miniconda3/envs/R_env/lib/R/library/")
-library(doParallel, lib.loc="miniconda3/envs/R_env/lib/R/library/")
-library(Rcpp, lib.loc="miniconda3/envs/R_env/lib/R/library/")
-library(RcppParallel, lib.loc="miniconda3/envs/R_env/lib/R/library/")
-library(RcppEigen, lib.loc="miniconda3/envs/R_env/lib/R/library/")
-library(rstan, lib.loc="miniconda3/envs/R_env/lib/R/library/") 
+# Load packages 
+
+
+## Include the "lib.loc" argument is necessary to tell R on the cluster where to look for the installed package
+library(optparse)
+library(doParallel, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library")
+library(Rcpp, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library")
+library(RcppParallel, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library")
+library(RcppEigen, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library")
+library(rstan, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library") 
 # the lib.loc argument is necessary to tell R on the cluster where to look for the installed package
     options(mc.cores = parallel::detectCores())
     rstan_options(auto_write = TRUE)
 #library(devtools)
 #install_github("gbradburd/conStruct",build_vignettes=TRUE) # Run once
-library(conStruct, lib.loc="miniconda3/envs/R_env/lib/R/library/")
+library(conStruct, lib.loc="/home/tcm0036/miniconda3/envs/R_env/lib/R/library")
+
+
+# Load input files 
 
 # Load allele frequency data
 #myconStructdata <- structure2conStruct(infile = "../populations.distichusonly.R0.7.noBahamas.str", 
 #                                       onerowperind = FALSE, start.loci = 3, 
 #                                       start.samples = 2, missing.datum = -9, 
 #                                       outfile = "myconStructdata")
+## Instead of using this function from conStruct, process the STRUCTURE file you obtain
+## from Stacks using the "structure-to-conStruct.R" script employing Emily Puckett's code
 
-# Use processed structure file obtained by running ".R"
+
+# Use processed structure file obtained by running "structure-to-conStruct.R"
 freq <- read.table("populations.distichusonly.R0.7.noBahamas.processed.str")
 freq <- as.matrix(freq)
 
 # Load geographic distance matrix generated with topoDistance package
-load(file = "distance-matrix/ddRAD_topoDistance_geog_mat.rda") # Update path here
+load(file = "ddRAD_topoDistance_geog_mat.rda")
 
 # Load lat. and long. for sampling localities
 latlong <- read.csv("ddRAD_cleaned_Hispaniolan_distichus_localities.csv", # Update path here
@@ -48,17 +57,30 @@ latlong <- read.csv("ddRAD_cleaned_Hispaniolan_distichus_localities.csv", # Upda
 latlong <- as.matrix(latlong[, c('Longitude', 'Latitude')])
 class(latlong)
 
+# Loop over values of K, performing both spatial and non-spatial runs
+          
+
+for(k in 1:10){
+
 # perform spatial analysis with conStruct
-spRun <- conStruct(spatial = TRUE,
-                  K =7, # update with value of K
+spRun.K <- conStruct(spatial = TRUE,
+                  K = as.numeric(k), # update with value of K
                   freqs = freq,
                   geoDist = tDist.mat,
                   coords = latlong,
-                  prefix = "spK7") # update with value of K
+		  n.chains = 8,
+		  save.files = TRUE,
+                  prefix = paste0("spK",k)) # update with value of K
 
 # perform non-spatial analysis
-nspRun <- conStruct(spatial = FALSE,
-                    freqs = freq,
+nspRun.K <- conStruct(spatial = FALSE,
+                    K = as.numeric(k),
+		    freqs = freq,
                     geoDist = NULL,
                     coords = coords,
-                    prefix = "nspK") # update with value of K
+		    n.chains = 8,
+		    save.files = TRUE,
+                    prefix = paste0("nspK",k)) # update with value of K
+
+}
+
--- population-structure/scripts/run-conStruct.sh
+++ population-structure/scripts/run-conStruct.sh
@@ -1,8 +1,18 @@
-#!usr/bin/env bash
-
-# Load conda environment with R
-source ~/miniconda3/etc/profile.d/conda.sh # Or path to where your conda is
-conda activate R_env
+#!/usr/bin/env bash
 
 # Submit R script as job
-Rscript conStruct_analysis.R
+
+	qsub -N conStruct-trial1 \
+		-n \
+		-d /home/tcm0036/distichus/population-structure/conStruct \
+		-q gen28 \
+		-W group_list=jro0014_lab \
+		-W x=FLAGS:ADVRES:jro0014_s28 \
+		-l nodes=1:ppn=10,mem=80gb,walltime=48:00:00 <<<"
+	
+	# Load conda environment with R
+	source ~/miniconda3/etc/profile.d/conda.sh # Or path to where your conda is
+	conda activate R_env
+
+	~/miniconda3/envs/R_env/bin/Rscript conStruct_analysis.R
+	"
