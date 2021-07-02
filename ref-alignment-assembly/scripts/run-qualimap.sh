20fd2f7daf75831b545818fc7353378207879f8c
Tanner C. Myers <tannercmyers@gmail.com>
2021-07-02 13:59:56 -0400

    Adding script to run Qualimap on sorted bam files

---
+++ ref-alignment-assembly/scripts/run-qualimap.sh
@@ -0,0 +1,6 @@
+
+
+#################################################################
+# This script assesses the mapping quality of .bam files output #
+# by "reference-alignment.sh" with the Java tool Qualimap       #
+#################################################################
