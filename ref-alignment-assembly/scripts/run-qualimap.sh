58535d4ed901e4330d6f37e5c58985d097184190
Tanner C. Myers <tannercmyers@gmail.com>
2021-07-07 16:05:52 -0500

    Adding line of code to run qualimap with the multi-bamqc flag

--- ref-alignment-assembly/scripts/run-qualimap.sh
+++ ref-alignment-assembly/scripts/run-qualimap.sh
@@ -1,6 +1,11 @@
-
+#! /usr/bin/env bash
 
 #################################################################
 # This script assesses the mapping quality of .bam files output #
 # by "reference-alignment.sh" with the Java tool Qualimap       #
 #################################################################
+# Load conda environment with bwa, samtools, and qualimap
+    source ~/mambaforge/etc/profile.d/conda.sh # Or path to where your conda is
+    conda activate genomics_env
+    	    	
+qualimap multi-bamqc --run-bamqc --data /scratch/phyletica/distichus/alignments/qualimap-input-information.tsv -gff /scratch/phyletica/distichus/genome/gtf/Anolis_carolinensis.AnoCar2.0v2.104.gtqualimap multi-bamqc --run-bamqc --data /scratch/phyletica/distichus/alignments/qualimap-input-information.tsv -gff /scratch/phyletica/distichus/genome/gtf/Anolis_carolinensis.AnoCar2.0v2.104.gtff
