f8fd3e4ac2269f6effeb36d6c9e461f2bff5b3ed
Tanner C. Myers <tannercmyers@gmail.com>
2021-07-07 14:26:50 -0500

    Adding Qualimap lines to script

--- ref-alignment-assembly/scripts/reference-alignment.sh
+++ ref-alignment-assembly/scripts/reference-alignment.sh
@@ -7,6 +7,10 @@
 # reference genome indexed by the script "bwa_index.sh". This 
 # will provide an alternative to Stacks for RAD loci assembly.
 
+#Note: Prior to running this script, users should create a 
+# conda environment with the packages involved here if they 
+# are not using their HPC's modules
+
 ################################################################
 
 # Designate sample ID variables
@@ -32,9 +36,11 @@
 
 for sample in $SAMPLES; 
     do
-        qsub -N $sample.refalign.test -d /scratch/phyletica/distichus/scripts -q gen28 -W group_list=jro0014_lab -W x=FLAGS:ADVRES:jro0014_s28 -l nodes=1:ppn=8,mem=80gb,walltime=12:00:00 <<<" 
+        # For each sample, submit a job to align the reads to the indexed genome with BWA MEM, convert to .bam and sort with samtools,
+        # and perform mapping QC with Qualimap.
+        qsub -N $sample.refalign -d /scratch/phyletica/distichus/scripts -q gen28 -W group_list=jro0014_lab -W x=FLAGS:ADVRES:jro0014_s28 -l nodes=1:ppn=8,mem=80gb,walltime=12:00:00 <<<" 
 
-            # Load conda environment with bwa
+            # Load conda environment with bwa, samtools, and qualimap
             source ~/mambaforge/etc/profile.d/conda.sh # Or path to where your conda is
             conda activate genomics_env
     	    	
@@ -44,7 +50,7 @@ for sample in $SAMPLES;
             READ2=$SAMPLE_DIR$sample.2.fq.gz
 
 	    # Align paired read fastq files to the indexed Anolis carolinensis reference, obtaining individual .sam files 
-            #bwa mem -t 8 /scratch/phyletica/distichus/genome/bwa/anocar $SAMPLE_DIR$sample.1.fq.gz $SAMPLE_DIR$sample.2.fq.gz -o $OUT_DIR/bwa-outputs/$sample.sam
+        bwa mem -t 8 /scratch/phyletica/distichus/genome/bwa/anocar $SAMPLE_DIR$sample.1.fq.gz $SAMPLE_DIR$sample.2.fq.gz -o $OUT_DIR/bwa-outputs/$sample.sam
 	    ## Should be bwa mem -t 8 $BWA_DB $READ1 $READ2 -o $OUT_DIR/bwa-outputs/$sample.sam
 
 	    # Use samtools to convert to .bam, sort, and index
@@ -52,5 +58,10 @@ for sample in $SAMPLES;
 	    samtools sort -o $OUT_DIR/bam-files/$sample.sorted.bam $OUT_DIR/bam-files/$sample.bam
 	    samtools index $OUT_DIR/bam-files/$sample.sorted.bam
 
+        # Use Qualimap to assess quality of mapping data
+        qualimap -bamqc \
+            -outdir $OUT_DIR/bam-files/qualimap \
+            -bam $OUT_DIR/bam-files/$sample.sorted.bam \
+            -gtf /scratch/phyletica/distichus/genome/gtf/Anolis_carolinensis.AnoCar2.0v2.104.gtf.gz
 	    "
     done
