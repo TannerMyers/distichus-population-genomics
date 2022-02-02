cf62f8fa40a3ba3c2775053002be467f80b03dad
Tanner C. Myers <tannercmyers@gmail.com>
2022-02-02 14:42:46 -0600

    Loop over deme sizes

--- population-structure/scripts/plot-EEMS-results.r
+++ population-structure/scripts/plot-EEMS-results.r
@@ -1,9 +1,30 @@
 setwd('/scratch/tcm0036/distichus-ddRAD/analyses/population-structure/eems/distichus-subsp/data')
 
 library(rEEMSplots)
-#library(reemsplots2)
+library(rgdal)
+library(rworldmap)
+library(reemsplots2)
 
-MCMCPATH = c('Hispaniola-distichus-ssp-pruned-EEMS-nDemes-chain1', 'Hispaniola-distichus-ssp-pruned-EEMS-nDemes-chain2', 'Hispaniola-distichus-ssp-pruned-EEMS-nDemes-chain3')
-PLOTPATH = 'plots/Hispaniola-distichus-subsp'
+map_world <-getMap()
 
-eems.plots(mcmcpath = MCMCPATH, plotpath = PLOTPATH, longlat = TRUE, out.png = FALSE)
+map_hispaniola <- map_world[c(which(map_world@data$SOVEREIGNT == "Dominican Republic"), which(map_world@data$SOVEREIGNT == "Haiti")), ]
+
+demeNumbers <- c(400,600,800,1200)              # Create vector to loop over
+
+for(i in demeNumbers){
+
+    MCMCPATH = c(paste0('Hispaniola-distichus-ssp-pruned-EEMS-nDemes',i,'-chain1'), 
+                paste0('Hispaniola-distichus-ssp-pruned-EEMS-nDemes',i,'-chain2'), 
+                paste0('Hispaniola-distichus-ssp-pruned-EEMS-nDemes',i,'-chain3'))
+
+    PLOTPATH = paste0('plots/Hispaniola-distichus-subsp-EEMS-nDemes', i,'-shapefile')
+
+
+    rEEMSplots::eems.plots(mcmcpath = MCMCPATH, plotpath = PLOTPATH, 
+            longlat = TRUE, out.png = FALSE, add.grid = TRUE, add.outline = TRUE,
+	        m.plot.xy = { plot(map_hispaniola, col = NA, add = TRUE)},
+	        q.plot.xy = { plot(map_hispaniola, col = NA, add = TRUE)})
+
+}
+
+ #reemsplots2::make_eems_plots(mcmcpath = MCMCPATH, longlat = TRUE)
