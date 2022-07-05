f425a4f76134fd37872fa4ecdb6c10d0019a2411
Tanner C. Myers <tannercmyers@gmail.com>
2022-07-04 22:56:02 -0500

    Adding conStruct updated to be run on cluster

--- population-structure/scripts/run-conStruct.r
+++ population-structure/scripts/run-conStruct.r
@@ -11,16 +11,24 @@ library(foreach)
 #########
 
 # (1) Load allele frequencies file
-    #structure2conStruct(infile = "distichus-spgroup-noheader-conStruct.str",
-    #    onerowperind = TRUE,
-    #    start.loci = 3,
-    #    missing.datum = 0, # This data matrix lacks any missing data so I just chose 0
-    #    outfile = "distichus-spgroup-conStuct-input")
-load("distichus-spgroup-conStuct-input.RData")
+    structure2conStruct(infile = "distichus-spgrp-North-Paleo-island-conStruct.str",
+        onerowperind = TRUE,
+        start.loci = 3,
+        missing.datum = 0, # This data matrix lacks any missing data so I just chose 0
+        outfile = "distichus-spgrp-North-Paleo-island-conStuct-input")
+load("distichus-spgrp-North-Paleo-island-conStuct-input.RData")
 
 # (2) Load coordinates for specimen sampling localities
-ind_data <- read_table("/home/tcm0036/distichus-ddRAD/info/all-distichoids-Hispaniola-only-cleaned-popmap.tsv", col_names = TRUE)
-latlong <- as.matrix(ind_data[,c('Longitude', 'Latitude')])
+RAD_data <- read_table("/home/tcm0036/distichus-ddRAD/info/distichus-popmap-cleaned-master.tsv", col_names = TRUE)
+RAD_data2 <- RAD_data %>% filter(Sample != "4481" & Sample != "10085" & Sample != "10086") %>%
+    filter(!(Taxon %in% c("websteri", "marron", "caudalis", "altavelensis"))) %>%
+    filter(Island %in% "Hispaniola")  %>%
+    filter(!grepl("_rep", Sample))
+
+snmfK9 <- read_table("../lea/sNMF_K9_ancestry_coefficients_cluster.tsv")
+ 
+RAD_data_north <- RAD_data2[RAD_data2$Sample_ID_pop %in% snmfK9$V1[snmfK9$cluster != 4 & snmfK9$cluster != 6 & snmfK9$cluster != 8],]
+latlong <- as.matrix(RAD_data_north[,c('Longitude', 'Latitude')])
 
 # (3) Generate geographic distance matrix from specimen sampling localities
 
@@ -30,17 +38,17 @@ latlong <- as.matrix(ind_data[,c('Longitude', 'Latitude')])
 geoDist <- fields::rdist.earth(x1 = latlong, x2 =latlong)
 
 #########
-# run conStuct analyses for Ks 1-10
+# run conStuct analyses for Ks 1-8
 #########
 
-dir.create("/mmfs1/scratch/tcm0036/distichus-ddRAD/analyses/population-structure/conStruct/outputs")
+dir.create("/mmfs1/scratch/tcm0036/distichus-ddRAD/analyses/population-structure/conStruct/north-island-outputs")
 
 #cl <- makeCluster(10, type = "FORK")
 #registerDoParallel(cl)
 
 myRun <- x.validation(train.prop = 0.9, 
     n.reps = 10,
-    K = 1:10,
+    K = 1:8,
     freqs = freqs,
     coords = latlong,
     geoDist = geoDist,
@@ -49,6 +57,6 @@ myRun <- x.validation(train.prop = 0.9,
     save.files = TRUE,
     parallel = TRUE,
     n.nodes = 10, 
-    prefix = "outputs/distichus-spgroup-CV")
+    prefix = "north-island-outputs/distichus-North-paleo-island-subsp_CV")
 
 #stopCluster(cl)
