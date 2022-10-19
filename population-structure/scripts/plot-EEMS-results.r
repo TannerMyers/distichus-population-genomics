bef0c6d9769fcb4fcfd49c8e7ca93fc70ce5ffba
Tanner C. Myers <tannercmyers@gmail.com>
2022-10-19 12:18:59 -0400

    Updating EEMS plotting

--- population-structure/scripts/plot-EEMS-results.r
+++ population-structure/scripts/plot-EEMS-results.r
@@ -4,7 +4,6 @@ library(rEEMSplots)
 library(rgdal)
 library(rgeos)
 library(raster)
-library(reemsplots2)
 
 DOM <- getData('GADM', country='DOM', level=0)
 HTI <- getData('GADM', country= 'HTI', level =0)
@@ -15,8 +14,8 @@ row.names(HTI) <- paste("HTI", row.names(HTI), sep="_")
 countryinfo <- rbind(HTI, DOM, makeUniqueIDs = TRUE)
 border <- gSimplify(countryinfo, tol=0.01, topologyPreserve=TRUE)
 
-
-demeNumbers <- c(400,600,800,1200)              # Create vector to loop over
+# Create vector to loop over
+demeNumbers <- c(400, 600, 800, 1200)
 
 for(i in demeNumbers){
 
@@ -27,11 +26,9 @@ for(i in demeNumbers){
     PLOTPATH = paste0('plots/Hispaniola-distichus-subsp-EEMS-nDemes', i,'-shapefile')
 
 
-    rEEMSplots::eems.plots(mcmcpath = MCMCPATH, plotpath = PLOTPATH, 
-            longlat = TRUE, out.png = FALSE, add.grid = TRUE, add.outline = TRUE,
-	        m.plot.xy = { plot(border, col = NA, add = TRUE)},
+    rEEMSplots::eems.plots(mcmcpath = MCMCPATH, plotpath = PLOTPATH,
+            longlat = TRUE, out.png = FALSE, 
+            add.grid = FALSE, add.outline = TRUE, 
+            m.plot.xy = { plot(border, col = NA, add = TRUE)},
 	        q.plot.xy = { plot(border, col = NA, add = TRUE)})
-
 }
-
- #reemsplots2::make_eems_plots(mcmcpath = MCMCPATH, longlat = TRUE)
