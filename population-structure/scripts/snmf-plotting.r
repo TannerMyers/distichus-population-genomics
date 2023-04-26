911f05fbfd1f244486c61e9935477d2bfb249d25
Tanner C. Myers <tannercmyers@gmail.com>
2023-04-26 14:00:57 -0500

    Adding conStruct interpretation of results

--- population-structure/scripts/snmf-plotting.r
+++ population-structure/scripts/snmf-plotting.r
@@ -1,168 +1,148 @@
-setwd("Dropbox/Distichus_Project")
+setwd("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/population-structure/AnoDist/geno100")
 
-library(sp)
-library(DescTools)
-library(gridExtra)
-library(raster)
-library(tiff)
-library(TeachingDemos)
-library(rgdal)
-library(RNetCDF)
-library(maps)
-library(mapdata)
-library(shapefiles)
-library(ncdf4)
-library(maptools)
-library(rgeos)
-library(beepr)
-library(ggmap)
-library(GISTools)
-library(ggplot2)
-library(graphics)
-library(prettymapr)
-library(extrafont)
 library(tidyverse)
 library(LEA)
+library(sf)
+library(terra)
+library(ggmap)
+library(marmap)
+library(scatterpie)
+library(ggnewscale)
 
 #################################################################################
-# Make base map
-bathy_fname <- "~/Dropbox/Distichus_Project/Mapping/distichus_hispaniola_map.nc" # from https://download.gebco.net/
-
-nc <- open.nc(bathy_fname)
-print.nc(nc)
-#plot(nc)
-
-tmp <- read.nc(nc)
-print.nc(nc)
-y <- tmp$lat
-y <- as.matrix(y, nrow=length(y), ncol=1)
-y<- y[order(y[,1], decreasing = F),]
-y <- as.matrix(y, nrow=length(y), ncol=1)
-x <- tmp$lon
-x <- as.matrix(x, nrow=length(x), ncol=1)
-z <- tmp$Elevation.relative.to.sea.level #layer
-rm(tmp)
-close.nc(nc)
-
-xlim <- range(x)
-ylim <- range(y)
-
-
-###Plot
-#make palette
-#ocean.pal <- colorRampPalette(c("#000000", "#000209", "#000413", "#00061E", "#000728", "#000932", "#002650", "#00426E", "#005E8C", "#007AAA", "#0096C8", "#22A9C2", "#45BCBB", "#67CFB5", "#8AE2AE", "#ACF6A8", "#BCF8B9", "#CBF9CA", "#DBFBDC", "#EBFDED")) 
-
-ocean.pal <- colorRampPalette(c("#064273","#1da2d8", "#def3f6", "#76b6c4","#7fcdff")) 
-
-land.pal <- colorRampPalette(
-  c("#336600", "#F3CA89", "#D9A627", 
-    "#A49019", "#9F7B0D", "#996600", "#B27676", "#C2B0B0", "#E5E5E5", 
-    "#FFFFFF")
-)
-
-#New land colors
-#land.pal <- colorRampPalette(c("#6F9C57","#A2BB74","#D9DF92","#F2E8A2","#F8DB93","#F3CE86","#DEAE84", "#C59781","#D29C97", "#E9C3D0","#FEF3F7"))
-
-#land.pal <- colorRampPalette(c("grey34","grey81"))
-zbreaks <- seq(-20000, 6000, by=10)
-cols <-c(ocean.pal(sum(zbreaks<=0)-1), land.pal(sum(zbreaks>0)))
 
 ## Load shapefile
-DOM <- getData('GADM', country = 'DOM', level=0, path = paste0("~/Dropbox/Distichus_Project/distichus-spatial-project/data/shape-files/"), download = F)
-HTI <- getData('GADM', country = 'HTI', level=0, path = paste0("~/Dropbox/Distichus_Project/distichus-spatial-project/data/shape-files/"), download = F)
+#border <- rgdal::readOGR("~/Dropbox/Distichus_Project/distichus-spatial-project/data/shape-files/Hispaniola.shp")
+border <- st_read(dsn = "~/Dropbox/Distichus_Project/distichus-spatial-project/data/shape-files/")
 
-row.names(DOM) <- paste("DOM", row.names(DOM), sep = "_")
-row.names(HTI) <- paste("HTI", row.names(HTI), sep = "_")
+alt <- rast("~/Dropbox/Distichus_Project/distichus-spatial-project/data/elevation_new/SRTM_elevation_1km.asc")
+    alt <- alt <- mask(alt, border)
+alt_df <- as.data.frame(alt, xy = T)
+    colnames(alt_df) <- c("x", "y", "elevation")
 
-countryinfo <- rbind(HTI, DOM, makeUniqueIDs = TRUE)
-border <- gSimplify(countryinfo, tol=0.01, topologyPreserve=TRUE)
+slope <- terrain(alt, "slope", unit = "radians")
+aspect <- terrain(alt, "aspect", unit = "radians")
+hill <- shade(slope, aspect, 45, 315)
+    names(hill) <- "shades"
+hill_df <- as.data.frame(hill, xy = TRUE)
+
+pal_grays <- hcl.colors(1000, "Grays")
+index <- hill %>%
+    mutate(index_col = scales::rescale(shades, to = c(1, length(pal_grays)))) %>%
+    mutate(index_col = round(index_col)) %>%
+    pull(index_col)
+vector_cols <- pal_grays[index]
+
+hill_plot <- ggplot() + geom_spatraster(data = hill, fill = vector_cols, maxcell = Inf, alpha = 1, show.legend = FALSE)
+
+basemap <- hill_plot +
+    geom_raster(data = alt_df, aes(x, y, fill = elevation), alpha = 0.7, show.legend = FALSE) +
+    scale_fill_hypso_tint_c(palette = "colombia", breaks = c(seq(0, 500, 50), seq(500, 2000, 500), 3000)) +
+    guides(fill = guide_colorsteps(barwidth = 20, barheight = .5)) +
+    theme_void() + theme(axis.line = element_blank(),
+    		axis.text.x = element_blank(),
+    		axis.text.y = element_blank(),
+    		axis.ticks = element_blank(),
+    		axis.title.x = element_blank(),
+    		axis.title.y = element_blank(),
+    		legend.position = "none",
+   		  	panel.grid.major = element_line(color = "white", size = 0.2),
+            panel.grid.minor = element_blank(),
+    		plot.title = element_text(size=18, color="grey20", hjust=1, vjust=-5),
+    		plot.caption = element_text(size=8, color="grey70", hjust=.15, vjust=20),
+    		plot.margin = unit(c(t=0, r=0, b=0, l=0), "lines"), #added these narrower margins to enlarge maps
+    		plot.background = element_rect(fill = "white", color = NA),
+    		panel.background = element_rect(fill = "white", color = NA),
+    		panel.border = element_blank())
+
+## Use Hispaniola shapefile and elevation raster to generate a topographic map in gggplot
+# basemap <- ggplot() + geom_tile(data = alt_df, aes(x = x, y = y, fill = elevation)) + scale_fill_etopo() +
+#     coord_sf(crs = "+proj=longlat +datum=WGS84 +no_defs") +
+#     geom_sf(data = border, fill = NA, color = "black") +
+#     theme_minimal() + theme(text = element_text(family = "georg", color = "#22211d"),
+#     		axis.line = element_blank(),
+#     		axis.text.x = element_blank(),
+#     		axis.text.y = element_blank(),
+#     		axis.ticks = element_blank(),
+#     		axis.title.x = element_blank(),
+#     		axis.title.y = element_blank(),
+#     		legend.position = "none",
+#    		  	panel.grid.major = element_line(color = "white", size = 0.2),
+#             panel.grid.minor = element_blank(),
+#     		plot.title = element_text(size=18, color="grey20", hjust=1, vjust=-5),
+#     		plot.caption = element_text(size=8, color="grey70", hjust=.15, vjust=20),
+#     		plot.margin = unit(c(t=0, r=0, b=0, l=0), "lines"), #added these narrower margins to enlarge maps
+#     		plot.background = element_rect(fill = "white", color = NA),
+#     		panel.background = element_rect(fill = "white", color = NA),
+#     		panel.border = element_blank()) + geom_point()
+
 #################################################################################
 
 # For optimal value of K, plot ancestry coefficients
-for (i in 5:10){
-    k = i
-    ce <- cross.entropy(obj.snmf, K = i) # K = 10 determined optimal
+    RAD_data_north <- read_table("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/info/distichus-north-island-popmap-master.tsv")
+    RAD_data_south <- read_table("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/info/distichus-south-island-popmap-master.tsv")
+    
+north_snmf <- load.snmfProject("lea/distichus/north/distichus-north-island-no-miss-50kbLD.snmfProject")
+    plot(north_snmf, cex = 1.2, col = "lightblue", pch = 19)
+    for (i in 1:6){
+        print(mean(cross.entropy(north_snmf, K = i)))
+    }
+    k <- 
+    ce <- cross.entropy(north_snmf, K = k)
     best_run <- which.min(ce)
-        print(paste0("The lowest cross entropy score for a K of ", i, " is ", ce[best_run]))
+        print(paste0("The lowest cross entropy score for a K of ", k, " is ", ce[best_run]))
 
-    qmatrix <- LEA::Q(obj.snmf, run = best_run, K = k)
-    Qmatrix  <- tess3r::as.qmatrix(qmatrix)
+    n_qmatrix <- LEA::Q(north_snmf, run = best_run, K = k)
+    Qmatrix  <- tess3r::as.qmatrix(n_qmatrix)
 
     # get cluster assignments per individual
     cluster <- apply(qmatrix, 1, which.max)
 
-    # Merge Qmatrix with specimen information
-    sNMF_ancestry_coefficients_cluster <- as.data.frame(cbind(RAD_data3[,c("Sample_ID_pop", "Locality", "Longitude", "Latitude")], cluster, qmatrix))
-    ## Order by cluster
-    snmf <- sNMF_ancestry_coefficients_cluster[order(as.numeric(sNMF_ancestry_coefficients_cluster$cluster)), ]
-        write_delim(x = snmf,
-            file = paste0("sNMF_K", k, "_ancestry_coefficients_cluster-specimen-cleaned.tsv"),
-            col_names = TRUE, delim = "\t")
+    n_df <- as_tibble(cbind(RAD_data_north[,c("Sample_ID_pop", "Locality", "Longitude", "Latitude")], cluster, n_qmatrix))
+        colnames(n_df) <- c("specimen", "locality", "longitude", "latitude", "cluster", "V1", "V2", "V3", "V4")
+        #write_delim(x = df, file = "", col_names = TRUE, delim = "\t")
+        n_df <- n_df[order(as.numeric(n_df$cluster)), ]
 
-    clusters <- grep("V", names(snmf))
-    
-    avg_admix <- aggregate(snmf[, clusters], by = snmf[, c("Locality", "Longitude", "Latitude")], mean)
-    #    avg_admix <- avg_admix[order(as.character(avg_admix$Locality)), ]
-    
-    png(paste0("snmf-pie-plot-K", k, ".pdf"))
-        raster::image(x,y,z=as.matrix(z), col=cols, breaks=zbreaks, useRaster = T,xlab = "", ylab = "", axes = F)
-        plot(border, add=T, lwd=0.2, border="black")
-        box(lwd=1.5)
-        scalebar(type = "bar", divs = 2, d = 200, below = "km", xy = NULL)
-        addnortharrow(scale = 0.35, pos = "topright")
-            for (i in 1:length(unique(avg_admix$Locality))){
-                mapplots::add.pie(z = as.numeric(avg_admix[i, 4:ncol(avg_admix)]),
-                                x = avg_admix[i, 2], y = avg_admix[i, 3],
-                                labels = "", col = paste0("cols_", k), radius = 0.07)
-            }
-        dev.off()
-}
+    n_colors <- c("sienna", "goldenrod1", "seashell4", "orangered")
+    barplot(t(n_df[, 6:ncol(n_df)]), col = n_colors, names.arg = n_df$specimen, las = 2, cex.names = 0.3, border = NA)
 
-k.5 <- read_table("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/population-structure/lea/sNMF/distichus-subsp/sNMF_K5_ancestry_coefficients_cluster-specimen-cleaned.tsv", 
-                    col_names = T)
-clusters <- grep("V", names(k.5))
-avg_admix <- aggregate(k.5[, clusters], by = k.5[, c("Locality", "Longitude", "Latitude")], mean)
-    avg_admix <- avg_admix[order(as.character(avg_admix$Locality)), ]
+    clusters <- grep("V", names(n_df))
+    n_avg_admix <- aggregate(n_df[, clusters], by = n_df[, c("locality", "longitude", "latitude")], mean)
 
-cols_5 <- c("orangered", "tan", "brown", "gray80", "gold")
-
-pdf("k.5.barplot.pdf", width = 18, height = 10)
-barplot(t(k.5[,3:7]), col = cols_5, names.arg = k.5$V1,
-        las = 2, cex.names = 0.3, border = NA)
-dev.off()
-
-pdf("non-hierarchical-k.5.barplot.pdf", width = 20, height = 10) # Change name depending
-barplot(t(snmf[,3:7]), col = cols_5, names.arg = snmf$Specimen,
-        las = 2, cex.names = 0.3, border = NA)
-dev.off()
+north <- basemap + new_scale_fill() +
+        geom_scatterpie(aes(x = longitude, y = latitude, group = locality), data = n_avg_admix, cols = colnames(n_avg_admix[, 4:ncol(n_avg_admix)]), alpha = 1) +
+        scale_fill_manual(values = n_colors)
+##############################################################################
 
 
-#pdf("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/population-structure/AnoDist/geno100/lea/distichus/hierarchical-snmf-K5-pie-map.pdf",
-pdf("~/Dropbox/Distichus_Project/ddRADseq_Phylogeography/population-structure/AnoDist/geno100/lea/distichus/non-hierarchical-snmf-K5-pie-map.pdf",
-    width = 14, height = 8)
-    par(mar=c(1,1,1,1))
-    raster::image(x,y,z=as.matrix(z), col=cols, breaks=zbreaks, useRaster=T,xlab="", ylab="",axes=F)
-    plot(border, add=T, lwd=0.2, border="black")
-    box(lwd=1.5)
-    scalebar(type = "bar", divs = 2, d = 200, below = "km", xy=NULL)
-    addnortharrow(scale = 0.35, pos = "topright")
-        # North paleo-island
-        for (i in 1:length(unique(avg_admix$Locality))){
-            mapplots::add.pie(z = as.numeric(avg_admix[i, 4:ncol(avg_admix)]),
-                            x = avg_admix[i, 2], y = avg_admix[i, 3],
-                            #labels = "", col = cols_5, radius = 0.05)
-                            labels = "", col = cols_5, radius = 0.05)
+south_snmf <- load.snmfProject("lea/distichus/south/distichus-south-island-no-miss-50kbLD.snmfProject")
+    plot(south_snmf, cex = 1.2, col = "lightblue", pch = 19)
+    for (i in 1:7){
+        print(mean(cross.entropy(south_snmf, K = i)))
         }
-dev.off()
+    k <- 3
+    ce <- cross.entropy(south_snmf, K = k)
+    best_run <- which.min(ce)
+        print(paste0("The lowest cross entropy score for a K of ", k, " is ", ce[best_run]))
 
-# pdf("snmf-k5-north-ordered.pdf", width = 16, height = 10)
-# barplot(t(test[, 3:ncol(test)]), col = c("orangered", "tan", "brown", "gray80", "gold"), names.arg = test$Specimen, las = 2, cex.names = 0.3, border = NA)
-# dev.off()
+    s_qmatrix <- LEA::Q(south_snmf, run = best_run, K = k)
+    Qmatrix  <- tess3r::as.qmatrix(s_qmatrix)
 
-        # South paleo-island    
-            for (i in 1:length(unique(s.avg_admix$Locality))){
-                mapplots::add.pie(z = as.numeric(s.avg_admix[i, 4:ncol(s.avg_admix)]),
-                                x = s.avg_admix[i, 2], y = s.avg_admix[i, 3],
-                                labels = "", col = c("blueviolet", "dodgerblue4"), radius = 0.045)
-            }
-#dev.off()
+    # get cluster assignments per individual
+    cluster <- apply(s_qmatrix, 1, which.max)
+
+    s_df <- as_tibble(cbind(RAD_data_south[,c("Sample_ID_pop", "Locality", "Longitude", "Latitude")], cluster, s_qmatrix))
+        colnames(s_df) <- c("specimen", "locality", "longitude", "latitude", "cluster", "V1", "V2", "V3")
+        #write_delim(x = df, file = "", col_names = TRUE, delim = "\t")
+        s_df <- s_df[order(as.numeric(s_df$cluster)), ]
+
+    s_colors <- c("dodgerblue", "burlywood", "darkorchid2")
+    barplot(t(s_df[, 6:ncol(s_df)]), col = s_colors, names.arg = s_df$specimen, las = 2, cex.names = 0.3, border = NA)
+
+    clusters <- grep("V", names(s_df))
+    s_avg_admix <- aggregate(s_df[, clusters], by = s_df[, c("locality", "longitude", "latitude")], mean)
+
+north + new_scale_fill() +
+    geom_scatterpie(aes(x = longitude, y = latitude, group = locality), data = s_avg_admix, cols = colnames(s_avg_admix[, 4:ncol(s_avg_admix)]), alpha = 1) +
+    scale_fill_manual(values = s_colors)
+ggsave("snmf-pie-plot-hierarchical.pdf")
