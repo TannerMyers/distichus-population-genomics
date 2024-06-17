400bc37491fefe2207e4504f0d29d181f49260a8
Tanner C. Myers <tannercmyers@gmail.com>
2024-06-17 14:34:56 -0500

    Updating and cleaning up ref-alignment-assembly directory

--- ref-alignment-assembly/scripts/bcftools-variant-calling.sh
+++ ref-alignment-assembly/scripts/bcftools-variant-calling.sh
@@ -2,22 +2,24 @@
 #SBATCH --job-name variant-calling
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=tcm0036@auburn.edu
-#SBATCH --time=3-24:00:00
-#SBATCH --cpus-per-task 8 
-#SBATCH --mem 80G
-#SBATCH --partition jro0014_amd
+#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task 16
+#SBATCH --mem 30G
+#SBATCH --partition bac0071_amd
+
+# Load conda environment containing R
+source ~/mambaforge/etc/profile.d/conda.sh # Or path to where your conda is
+conda activate genomics_env
 
 # Must be bgzipped if compressed
 genome=/home/tcm0036/distichus-ddRAD/genome/Anolis_distichus/AnoDis1.0.fasta
-popmap=/home/tcm0036/distichus-ddRAD/info/popmap-bcftools.tsv
-outputfile=/scratch/tcm0036/distichus-ddRAD/alignments/AnoDist/bcf/variants.bcf
-tmp=mapped-files.tmp.list
+popmap=/home/tcm0036/distichus-ddRAD/info/distichus-popmap-bcftools.tsv
+outputfile=/scratch/tcm0036/distichus-ddRAD/alignments/AnoDist/bcf/distichus_variants.bcf 
+tmp=mapped-files.list.tmp
 
-ls /scratch/tcm0036/distichus-ddRAD/alignments/AnoDist/bam/*sorted.bam > $tmp 
+awk '{print $1}' $popmap > $tmp
 
-module load bcftools
-
-bcftools mpileup -Ou -a AD --fasta-ref $genome --bam-list $tmp | \
+bcftools mpileup -Ou --annotate AD,DP --fasta-ref $genome --bam-list $tmp | \
 bcftools call --variants-only --threads $SLURM_CPUS_PER_TASK --multiallelic-caller --group-samples $popmap -Ob -o $outputfile
 
-rm $tmp
+#rm $tmp
